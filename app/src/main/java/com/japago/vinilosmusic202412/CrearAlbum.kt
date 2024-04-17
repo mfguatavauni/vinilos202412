527c30432c1d5a38d5196c7a7bf6c6096caec1ec
John Alexander Pardo Gonzalez <j.pardog@uniandes.edu.co>
2024-04-17 17:35:15 -0500

    Se agrega vista de crear album

--- app/src/main/java/com/japago/vinilosmusic202412/CrearAlbum.kt
+++ app/src/main/java/com/japago/vinilosmusic202412/CrearAlbum.kt
@@ -1,30 +1,20 @@
 package com.japago.vinilosmusic202412
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CrearAlbum : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_crear_album)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val btnCrearAlbum = findViewById<Button>(R.id.btnCrearAlbum)
-
-        btnCrearAlbum.setOnClickListener {
-            val intent = Intent(this, CrearAlbum::class.java)
-            startActivity(intent)
-        }
-
     }
 }
